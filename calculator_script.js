3739e027b2183c531ae56e63f888401d4b287bcb
HJ314159 <102838854+HJ314159@users.noreply.github.com>
2023-10-29 10:06:50 +0000

    Configured increase piggy size function

--- calculator_script.js
+++ calculator_script.js
@@ -9,16 +9,6 @@ let savingGoal = 0;
 let dailySave = 0;
 let saveTime = 0;
 
-// let minus = document.getElementById("minus");
-// let add = document.getElementById("add");
-// let piggy = document.getElementById("real-piggy")
-// let currentPiggySize = window.getComputedStyle(document.getElementById("real-piggy")).fontSize;
-
-const smallIcon = document.getElementById("small-piggy");
-const bigIcon = document.getElementById("big-piggy");
-const addBtn = document.getElementById("add");
-const clicksInput = document.getElementById("clicks-input");
-
 document.addEventListener("DOMContentLoaded", function(){
     sendBtn.addEventListener("click", function(){
         send();
@@ -37,24 +27,16 @@ document.addEventListener("DOMContentLoaded", function(){
             dailySave = parseFloat(inputValue); 
             saveTime = savingGoal / dailySave;
             botMessage(`${saveTime} days is what you'll need to save $${savingGoal}. Good luck!`);
-            let piggySize = saveTime / 15;
-
-            addBtn.addEventListener("click", () => {
-                const clicks = parseInt(clicksInput.value, 10)
-                const currentSize = parseInt(getComputedStyle(smallIcon).fontSize, 10);
-                console.log(currentSize)
-
-                if (currentSize < parseInt(getComputedStyle(bigIcon).fontSize, 10)){
-                    const newSize = currentSize + clicks;
-                    smallIcon.style.fontSize = newSize + "px";
-                };
-            });
-        }
+            }
     }
     
     function botMessage(message) {
         let div = document.createElement("DIV");
         div.innerHTML = message; 
         conversation[0].appendChild(div); 
-    };
+    }
+
+    sessionStorage.setItem('userTime', saveTime);
+    sessionStorage.setItem('userGoal', savingGoal);
 });
+
